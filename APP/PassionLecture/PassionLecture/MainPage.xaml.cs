758caf6c50c3253ddc17b38181039696bf1e7267
antoineFabr <163823977+antoineFabr@users.noreply.github.com>
2025-03-28 15:38:58 +0100

    Création du projet MAUI

---
+++ APP/PassionLecture/PassionLecture/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace PassionLecture
+{
+    public partial class MainPage : ContentPage
+    {
+       
+
+        public MainPage()
+        {
+            InitializeComponent();
+            pickerNumber.ItemsSource = new List<string>
+            {
+                "1",
+                "2",
+                "3",
+                "4",
+            };
+        }
+
+      
+    }
+
+}
